9cf0d145abaffef764105e348858089bdb85687a
Henk Wietsma <151195189+HenkW00@users.noreply.github.com>
2024-02-10 23:33:00 +0100

    Update version.lua

--- server/version.lua
+++ server/version.lua
@@ -47,6 +47,7 @@ if Config.checkForUpdates then
             else
                 repoVersion = curVersion
                 repoURL = "https://github.com/HenkW00/hw_weed"
+                print('^0[^3WARNING^0] Could ^1NOT^0 verify latest version from ^5github^0!')
             end
         end, "GET")
 
